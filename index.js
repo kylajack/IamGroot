e48f0c39a10c99bc65b5f88e6aa1a779db25c5e6
Kyla Jackson <kyla.cjackson820@gmail.com>
2020-07-17 10:42:40 -0400

    added a jumbotron, nav bar, alerts, changed color of jumbotron and nav bar

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var k = "to myself"
+var y = 2000000000000
+var l = "and clouds of white"
+var a = true
+var c = 5
+alert("and I think\n" + k)
+alert("How many people are in the world?\n" + y * c)
+alert("Isn't it a wonderful world?\n" + a)
+alert("I see skies of blue\n" + l)
+var planetnames = ["Mars","Jupiter", "Saturn", "Earth", "Neptune"];
+console.log(planetnames[0]);
+console.log(planetnames[4]);
+console.log(planetnames[2]);
+console.log(planetnames[1]);
+console.log(planetnames.length);
+console.log(planetnames.length - 2);
+
